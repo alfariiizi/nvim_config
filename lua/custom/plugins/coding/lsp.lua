34ad8fc7455b6fee8eae795a8932a2ec53cf9007
Moh Rizal Alfarizi <alfariiiziiiii@gmail.com>
2024-04-19 15:38:27 +0700

    keymap for show diagnostic and list diagnostic in current buffer

--- lua/custom/plugins/coding/lsp.lua
+++ lua/custom/plugins/coding/lsp.lua
@@ -104,6 +104,9 @@ return {
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          map('<leader>xd', vim.diagnostic.open_float, 'Show [D]iagnostic')
+          map('<leader>xD', '<cmd>Telescope diagnostics bufnr=0<cr>', 'Buffer [D]iagnostic')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
