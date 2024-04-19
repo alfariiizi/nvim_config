0eeb8bb1b5b43dc47dbf86980ebe0a50e8ecd795
Moh Rizal Alfarizi <alfariiiziiiii@gmail.com>
2024-04-19 15:37:25 +0700

    add lsp file operation
    Example: I have Content.tsx and it is used in page.tsx, when I rename to PageContent.tsx then the page.tsx import will be updated

--- lua/custom/plugins/autocompletion.lua
+++ lua/custom/plugins/autocompletion.lua
@@ -33,7 +33,13 @@ return {
       -- Adds other completion capabilities.
       --  nvim-cmp does not ship with all sources by default. They are split
       --  into multiple repos for maintenance purposes.
-      'hrsh7th/cmp-nvim-lsp',
+      {
+        'hrsh7th/cmp-nvim-lsp',
+        dependencies = {
+          'antosha417/nvim-lsp-file-operations',
+          opts = {},
+        },
+      },
       'hrsh7th/cmp-path',
 
       -- For colorizing tailwind in completion popup
