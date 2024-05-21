841f5d416d6a83ef6ec160787695e50d3d73f6e5
Moh Rizal Alfarizi <alfariiiziiiii@gmail.com>
2024-05-21 10:53:14 +0700

    feat: add debugprint plugin

---
+++ lua/custom/plugins/debugprint.lua
@@ -0,0 +1,40 @@
+local leader = '<leader>p'
+
+return {
+  'andrewferrier/debugprint.nvim',
+  dependencies = {
+    'echasnovski/mini.nvim', -- Needed to enable :ToggleCommentDebugPrints for NeoVim 0.9
+  },
+  -- Remove the following line to use development versions,
+  -- not just the formal releases
+  version = '*',
+  opts = {
+    keymaps = {
+      normal = {
+        plain_below = leader .. 'p',
+        plain_above = leader .. 'P',
+        variable_below = leader .. 'v',
+        variable_above = leader .. 'V',
+        variable_below_alwaysprompt = leader .. 'a',
+        variable_above_alwaysprompt = leader .. 'A',
+        textobj_below = leader .. 'o',
+        textobj_above = leader .. 'O',
+        toggle_comment_debug_prints = leader .. 'c',
+        delete_debug_prints = leader .. 'd',
+      },
+      visual = {
+        variable_below = leader .. 'v',
+        variable_above = leader .. 'V',
+      },
+    },
+    commands = {
+      toggle_comment_debug_prints = 'ToggleCommentDebugPrints',
+      delete_debug_prints = 'DeleteDebugPrints',
+    },
+  },
+  config = function()
+    require('which-key').register {
+      [leader] = { name = '[P]rint to Debug', _ = 'which_key_ignore' },
+    }
+  end,
+}
