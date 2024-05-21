6b74a3da0e0d077e4be6a3da28c1d95a3e38f776
Moh Rizal Alfarizi <alfariiiziiiii@gmail.com>
2024-05-21 11:58:17 +0700

    fix: texlab_ls config inside lua_ls

--- lua/custom/plugins/coding/lsp.lua
+++ lua/custom/plugins/coding/lsp.lua
@@ -172,11 +172,11 @@ return {
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
             },
-            texlab = {
-              keys = {
-                { '<Leader>K', '<plug>(vimtex-doc-package)', desc = 'Vimtex Docs', silent = true },
-              },
-            },
+          },
+        },
+        texlab = {
+          keys = {
+            { '<Leader>K', '<plug>(vimtex-doc-package)', desc = 'Vimtex Docs', silent = true },
           },
         },
       }
