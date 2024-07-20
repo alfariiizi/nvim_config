a3b61fe583b0c5d22da228e73a0d4dd177e58d32
Moh Rizal Alfarizi <alfariiiziiiii@gmail.com>
2024-07-20 15:35:38 +0700

    fix: markdown renderer options

--- lua/custom/plugins/lang/markdown.lua
+++ lua/custom/plugins/lang/markdown.lua
@@ -4,12 +4,36 @@ return {
   {
     'MeanderingProgrammer/markdown.nvim',
     name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
     enabled = true,
     config = function()
       require('render-markdown').setup {
-        headings = { '󰉫 ', '󰉬 ', '󰉭 ', '󰉮 ', '󰉯 ', '󰉰 ' },
-        bullets = { '•' },
+        heading = {
+          -- Turn on / off heading icon & background rendering
+          enabled = true,
+          -- Turn on / off any sign column related rendering
+          sign = true,
+          -- Replaces '#+' of 'atx_h._marker'
+          -- The number of '#' in the heading determines the 'level'
+          -- The 'level' is used to index into the array using a cycle
+          -- The result is left padded with spaces to hide any additional '#'
+          icons = { '󰉫 ', '󰉬 ', '󰉭 ', '󰉮 ', '󰉯 ', '󰉰 ' },
+          -- Added to the sign column if enabled
+          -- The 'level' is used to index into the array using a cycle
+          signs = { '󰫎 ' },
+          -- The 'level' is used to index into the array using a clamp
+        },
+        bullet = {
+          -- Turn on / off list bullet rendering
+          enabled = true,
+          -- Replaces '-'|'+'|'*' of 'list_item'
+          -- How deeply nested the list is determines the 'level'
+          -- The 'level' is used to index into the array using a cycle
+          -- If the item is a 'checkbox' a conceal is used to hide the bullet instead
+          icons = { '•', '•', '-', '-' },
+          -- Highlight for the bullet icon
+          highlight = 'RenderMarkdownBullet',
+        },
         win_options = {
           -- See :h 'conceallevel'
           conceallevel = {
