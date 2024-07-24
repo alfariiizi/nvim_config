be3a98f0cb7a702c1148877d7dcef381824742bb
Moh Rizal Alfarizi <alfariiiziiiii@gmail.com>
2024-07-24 10:59:39 +0700

    fix: conform setup

--- lua/custom/plugins/coding/formatter.lua
+++ lua/custom/plugins/coding/formatter.lua
@@ -4,58 +4,97 @@ return {
     'stevearc/conform.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
     lazy = false,
-    keys = {
-      {
-        '<leader>cf',
-        function()
-          require('conform').format { async = true, lsp_fallback = true }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
-    opts = {
-      format = {
-        -- It's from lazyvim
-        timeout_ms = 3000,
-        async = false, -- not recommended to change
-        quiet = false, -- not recommended to change
-        lsp_fallback = true, -- not recommended to change
-      },
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+    -- keys = {
+    --   {
+    --     '<leader>cf',
+    --     function()
+    --       require('conform').format { async = true, lsp_fallback = true }
+    --     end,
+    --     mode = '',
+    --     desc = '[F]ormat buffer',
+    --   },
+    -- },
+    config = function()
+      local conform = require 'conform'
+
+      conform.setup {
+        notify_on_error = false,
+        formatters_by_ft = {
+          lua = { 'stylua' },
+          python = { 'isort', 'black' },
+          javascript = { 'prettierd', 'prettier' },
+          typescript = { 'prettierd', 'prettier' },
+          javascriptreact = { 'prettierd', 'prettier' },
+          typescriptreact = { 'prettierd', 'prettier' },
+          svelte = { 'prettierd', 'prettier' },
+          css = { 'prettierd', 'prettier' },
+          html = { 'prettierd', 'prettier' },
+          json = { 'prettierd', 'prettier' },
+          yaml = { 'prettierd', 'prettier' },
+          ['markdown'] = { 'prettierd', 'prettier', 'markdownlint', 'markdown-toc' },
+          ['markdown.mdx'] = { 'prettierd', 'prettier', 'markdownlint', 'markdown-toc' },
+          graphql = { 'prettierd', 'prettier' },
+          tex = { 'latexindent' },
+          plaintex = { 'latexindent' },
+        },
+        format_on_save = {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        },
+      }
+
+      vim.keymap.set({ 'n', 'v' }, '<leader>cf', function()
+        conform.format {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
         }
-      end,
-      ---@type table<string, conform.FormatterUnit[]>
-      formatters_by_ft = {
-        lua = { { 'stylua' } },
-        -- Conform can also run multiple formatters sequentially
-        python = { { 'isort', 'black' } },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        javascript = { { 'prettierd', 'prettier' } },
-        typescript = { { 'prettierd', 'prettier' } },
-        javascriptreact = { { 'prettierd', 'prettier' } },
-        typescriptreact = { { 'prettierd', 'prettier' } },
-        svelte = { { 'prettierd', 'prettier' } },
-        css = { { 'prettierd', 'prettier' } },
-        html = { { 'prettierd', 'prettier' } },
-        json = { { 'prettierd', 'prettier' } },
-        yaml = { { 'prettierd', 'prettier' } },
-        ['markdown'] = { { 'prettierd', 'prettier' }, 'markdownlint', 'markdown-toc' },
-        ['markdown.mdx'] = { { 'prettierd', 'prettier' }, 'markdownlint', 'markdown-toc' },
-        graphql = { { 'prettierd', 'prettier' } },
-        tex = { { 'latexindent' } },
-        plaintex = { { 'latexindent' } },
-      },
-    },
+      end, { desc = '[F]ormat buffer' })
+    end,
+
+    -- opts = {
+    --   format = {
+    --     -- It's from lazyvim
+    --     timeout_ms = 3000,
+    --     async = false, -- not recommended to change
+    --     quiet = false, -- not recommended to change
+    --     lsp_fallback = true, -- not recommended to change
+    --   },
+    --   notify_on_error = false,
+    --   format_on_save = function(bufnr)
+    --     -- Disable "format_on_save lsp_fallback" for languages that don't
+    --     -- have a well standardized coding style. You can add additional
+    --     -- languages here or re-enable it for the disabled ones.
+    --     local disable_filetypes = { c = true, cpp = true }
+    --     return {
+    --       timeout_ms = 500,
+    --       lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+    --     }
+    --   end,
+    --   ---@type table<string, conform.FormatterUnit[]>
+    --   formatters_by_ft = {
+    --     lua = { { 'stylua' } },
+    --     -- Conform can also run multiple formatters sequentially
+    --     python = { { 'isort', 'black' } },
+    --     --
+    --     -- You can use a sub-list to tell conform to run *until* a formatter
+    --     -- is found.
+    --     javascript = { { 'prettierd', 'prettier' } },
+    --     typescript = { { 'prettierd', 'prettier' } },
+    --     javascriptreact = { { 'prettierd', 'prettier' } },
+    --     typescriptreact = { { 'prettierd', 'prettier' } },
+    --     svelte = { { 'prettierd', 'prettier' } },
+    --     css = { { 'prettierd', 'prettier' } },
+    --     html = { { 'prettierd', 'prettier' } },
+    --     json = { { 'prettierd', 'prettier' } },
+    --     yaml = { { 'prettierd', 'prettier' } },
+    --     ['markdown'] = { { 'prettierd', 'prettier' }, 'markdownlint', 'markdown-toc' },
+    --     ['markdown.mdx'] = { { 'prettierd', 'prettier' }, 'markdownlint', 'markdown-toc' },
+    --     graphql = { { 'prettierd', 'prettier' } },
+    --     tex = { { 'latexindent' } },
+    --     plaintex = { { 'latexindent' } },
+    --   },
+    -- },
   },
 }
